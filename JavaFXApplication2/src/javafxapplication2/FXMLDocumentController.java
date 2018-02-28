256e34e9f67ebd25e52015cfdf0fa99f9e9ef8d8
Dmitry <defrakcija123@gmail.com>
2018-02-28 16:21:27 -0700

    New one

--- JavaFXApplication2/src/javafxapplication2/FXMLDocumentController.java
+++ JavaFXApplication2/src/javafxapplication2/FXMLDocumentController.java
@@ -1,6 +1,6 @@
-
 package javafxapplication2;
 
+import com.jfoenix.controls.*;
 import java.net.URL;
 import static java.nio.file.Files.list;
 import static java.rmi.Naming.list;
@@ -14,89 +14,82 @@ import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.animation.FadeTransition;
-import javafx.animation.KeyFrame;
-import javafx.animation.PathTransition;
-import javafx.animation.ScaleTransition;
-import javafx.animation.SequentialTransition;
-import javafx.animation.Timeline;
+import javafx.animation.*;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.shape.CubicCurveTo;
-import javafx.scene.shape.LineTo;
-import javafx.scene.shape.MoveTo;
-import javafx.scene.shape.Path;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.*;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 import javafx.util.Duration;
+import javax.swing.Icon;
 
 /**
  *
  * @author 719454
  */
 public class FXMLDocumentController implements Initializable {
-    public int count = 0;
-    
+
     @FXML
     private Label lblLondon, lblRiga, lblParis, lblBerlin, lblMoscow, lblBrussel, lblCalgary;
     @FXML
-    private Button button;
+    private Button btnLogin;
+
+   // @FXML
+   // private JX txtUsername;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-     
+    private PasswordField txtPassword;
+    
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    private void btnLoginAction(ActionEvent event) {
+        lblRiga.setText("Done!");
     }
     
+    @FXML
+    private void iconCloseAction(ActionEvent event) {
+        Stage stage = (Stage) closeIcon.getScene().getWindow();
+        stage.close();
+    }
+    
+    @FXML
+    private Label closeIcon;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         List<Label> labels = new ArrayList<>(
-        Arrays.asList(lblLondon, lblRiga, lblParis, lblBerlin, lblMoscow, lblBrussel, lblCalgary));
-       
-           
-        Timeline anim = new Timeline();
-        anim.setCycleCount(50); // Animation will play 50 times
-        
-        int seconds = 1;
-        for(int i = 0; i < labels.size(); i++){
-            setTimer(i, labels.get(i));
-        }
-        }
-        public void setTimer(int seconds, Label l) {
-         Timer timer = new Timer();
-         timer.schedule(new TimerTask() {
-             @Override
-             public void run() {
-                 moveLabel(l);
-             }
-         }, (seconds*1000));       
-}
-       
-    
-    private void moveLabel(Label lbl){
-        System.out.println("foo" + count);
+                Arrays.asList(lblLondon, lblRiga, lblParis, lblBerlin, lblMoscow, lblBrussel, lblCalgary));
+        lblRiga.setText(String.valueOf(lblRiga.getLayoutX()));
+        moveLabel(lblRiga, 0, 75);
+        moveLabel(lblParis, 50, 80);
+    }
+
+    private void moveLabel(Label lbl, int posX, int posY) {
         PathTransition pathTransition = new PathTransition();
-        //lbl.setLayoutX(25);
-      //  lbl.setLayoutY(0);
+        //lbl.setLayoutX(50);
+        //lbl.setLayoutY(65);
         Path path = new Path();
-        path.getElements().add(new MoveTo(25,-50));
-        path.getElements().add(new LineTo(25, 350));
-        
+        path.getElements().add(new MoveTo(posX, posY));
+        path.getElements().add(new LineTo(50, 350));
+
         pathTransition.setDuration(Duration.seconds(10));
         pathTransition.setPath(path);
         pathTransition.setNode(lbl);
         pathTransition.setCycleCount(Timeline.INDEFINITE);
-        
-        
+
         FadeTransition fadeIn = new FadeTransition(Duration.seconds(5), lbl);
-        fadeIn.setFromValue(0);
+        fadeIn.setFromValue(0.5);
         fadeIn.setToValue(1);
         fadeIn.setAutoReverse(true);
         pathTransition.play();
         fadeIn.play();
-        System.out.println("bar" + count);
-        count++;
     }
-    
+
 }
