6069003366ec509918b40bd091f2d407f95f9237
ACDM\719454 <defrakcija123@gmail.com>
2018-03-08 20:24:35 -0700

    Updated Projects

--- TravelExpertsFX/src/application/Main.java
+++ TravelExpertsFX/src/application/Main.java
@@ -1,20 +1,30 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
 
 
 public class Main extends Application {
+	
+	public static Stage pstage;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/view/Login.fxml"));
+			//Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.initStyle(StageStyle.UNDECORATED);
 			primaryStage.show();
+			pstage = primaryStage;
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
